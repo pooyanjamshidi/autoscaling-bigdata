93aa314fb5abc0601b1f3c33803e608d93a194a7
Pooyan Jamshidi <pooyan.jamshidi@gmail.com>
2016-05-23 23:45:26 +0100

    added topology deployment method

---
+++ storm/deploy_storm_topology.m
@@ -0,0 +1,12 @@
+function [status]=deploy_storm_topology(topology_config_name)
+% Submit a topology with a specific configuration file.
+
+% Authors: Pooyan Jamshidi (pooyan.jamshidi@gmail.com)
+% The code is released under the FreeBSD License.
+% Copyright (C) 2016 Pooyan Jamshidi, Imperial College London
+
+% TODO 
+
+
+
+end
