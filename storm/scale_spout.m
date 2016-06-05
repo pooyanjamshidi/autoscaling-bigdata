7ea0327c2cee6e7aec774a367bee2d5d83c29030
Pooyan Jamshidi <pooyan.jamshidi@gmail.com>
2016-06-06 00:47:49 +0100

    editted spout scale

--- storm/scale_spout.m
+++ storm/scale_spout.m
@@ -1,13 +1,12 @@
-function [status,msg]=scale_spout(deployment_id,parallelism)
-% scale a topology after checking invariants that may be violated
+function [status,msg]=scale_spout(deployment_id,spout_parallelism)
+% scale the spout of topologies after checking invariants that may be violated
 % if no invariant is violated it then scale the topology by calling
 % rebalance function
 
 % example: scale_spout('RollingSort-57-1463841798',3)
 % 'RollingSort-57-1463841798' is the topology id,
-% struct('spout',1,'sort',2) provides the paralelism level of all
-% components including all spouts and bolts, use the id of the components
-% in the struct
+% spout_parallelism provides the paralelism level of all
+% spout components
 
 % regarding invariants, see: http://www.michael-noll.com/blog/2012/10/16/understanding-the-parallelism-of-a-storm-topology/
 
@@ -31,28 +30,14 @@ wait_time=topology_stats.msgTimeout;
 
 status=1; % rebalance can be done by default
 
-component_ids=fieldnames(parallelism);
+parallelism={};
 
-for i=1:size(component_ids,1)
-    
-    % search spouts
-    for j=1:size(topology_stats.spouts,1)
-        if strcmp(component_ids(i),topology_stats.spouts(j).spoutId)
-            if getfield(parallelism, char(component_ids(i))) > topology_stats.spouts(j).tasks
-                status=0; % it violates the invariant: #executors <= #numTasks
-                break
-            end
-        end
-    end
-    
-    % search bolts
-    for j=1:size(topology_stats.bolts,1)
-        if strcmp(component_ids(i),topology_stats.bolts(j).boltId)
-            if getfield(parallelism, char(component_ids(i))) > topology_stats.bolts(j).tasks
-                status=0; % it violates the invariant: #executors <= #numTasks
-                break
-            end
-        end
+% search spouts
+for j=1:size(topology_stats.spouts,1)
+    parallelism=setfield(parallelism,topology_stats.spouts.spoutId,spout_parallelism);
+    if spout_parallelism > topology_stats.spouts(j).tasks
+        status=0; % it violates the invariant: #executors <= #numTasks
+        break
     end
 end
 
